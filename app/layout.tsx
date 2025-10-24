ecb4e57f58db1ec32fcb01f4a8c9ccfe90fc48e0
Yaniv <yanivdouieb@icloud.com>
2025-10-24 12:40:59 +0200

    feat(layout): add RootLayout and initial page structure

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+
+const metadata: Metadata = {
+  title: "Intend - Billing solution for school daycare centers",
+};
+
+const RootLayout = ({
+    children,
+    }: {
+    children: React.ReactNode;
+    }) => {
+  return (
+    <html lang={"en"}>
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export { metadata }
+export default RootLayout;
